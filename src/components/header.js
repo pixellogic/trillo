9891ccd2acdf1acb0ed5f7120ccd7dce78d85854
Ashish Bajracharya <lsr.files1@gmail.com>
2021-01-18 21:57:28 +0545

    header section completed

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearchLocation,faBookmark, faComment } from '@fortawesome/free-solid-svg-icons'
+import profile from '../images/me.png'
 export default function Header() {
     return (
         <div className = "header">
-            header
+            <div className = "header__logo">trillo</div>
+                <form action = "#" className = "header__search">
+                    <input type = "text" placeholder = "search" className = "header__search__input" />
+                    <button className = "header__search__button">
+                    <FontAwesomeIcon icon={faSearchLocation} />
+                    </button>
+                </form>
+                <div className = "nav">
+                    <div className = "nav__icon-box">
+                        <FontAwesomeIcon icon = {faBookmark} />
+                        <span className ="nav__notification" >7</span>
+                    </div>
+                    <div className = "nav__icon-box">
+                        <FontAwesomeIcon icon = {faSearchLocation} />
+                        <span className ="nav__notification" >7</span>
+                    </div>
+                    <div className = "nav__icon-box">
+                        <FontAwesomeIcon icon = {faComment} />
+                        <span className ="nav__notification" >777</span>
+                    </div>
+                    <div className = "nav__user">
+                        <img src = {profile} className = "nav__user__photo" />
+                        Ashish
+                    </div>
+                </div>
+            
         </div>
     )
 }
